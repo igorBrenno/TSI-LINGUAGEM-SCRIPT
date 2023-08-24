7c5a70e654f995fda5f7607f3871abe2261d22d9
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2023-08-24 15:20:00 +0000

    crete the funtion in javascript

--- aulas-linguagem-script/aula2/js/script.js
+++ aulas-linguagem-script/aula2/js/script.js
@@ -1,1 +1,8 @@
-console.log("Funcionando")
+
+
+function clicando() {
+    var numero1 = document.getElementById("primeiro").value;
+    var numero2 = document.getElementById("segundo").value;
+    var resultado = parseInt(numero1) + parseInt(numero2);
+    console.log(resultado)
+}
